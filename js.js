08495f1c6bd04489c09187c33d981a0e4b0f8fd2
Ritik Bhola <ritikbhola08@gmail.com>
2022-07-13 18:40:08 +0530

    updated

--- js.js
+++ js.js
@@ -44,4 +44,17 @@ const saveTheme = (theme) => {
     const themeSwitch = document.querySelector(".toggle-theme");
     themeSwitch.onclick = () => toggleTheme();
   };
+
+
+/** CLOSE MAIN NAVIGATION WHEN CLICKING OUTSIDE THE MAIN NAVIGATION AREA**/
+$(document).on('click', function (e){
+    /* bootstrap collapse js adds "in" class to your collapsible element*/
+    var menu_opened = $('#main-navigation').hasClass('in');
   
+    if(!$(e.target).closest('#main-navigation').length &&
+        !$(e.target).is('#main-navigation') &&
+        menu_opened === true){
+            $('#main-navigation').collapse('toggle');
+    }
+
+});
